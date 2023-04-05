10149e9e8ba89fa960c8e6a431369d82129319d7
AlexMindr <49548048+AlexMindr@users.noreply.github.com>
2023-04-05 22:22:19 +0300

    modal + add client mutation

--- client/src/components/Clients.tsx
+++ client/src/components/Clients.tsx
@@ -1,4 +1,4 @@
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import ClientRow from "@/components/ClientRow";
 import { ClientType } from "@/types/types";
 import { GET_CLIENTS } from "@/queries/clientQueries";
